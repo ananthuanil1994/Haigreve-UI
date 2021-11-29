593edcd474f2c7eaa43458dcdf8f79e49987714f
Jagadeeshlal <jagadeeshlal@factweavers.com>
2021-11-29 15:38:42 +0530

    style update

--- src/Routes/Home/Success.js
+++ src/Routes/Home/Success.js
@@ -1,33 +1,40 @@
+/* eslint-disable */
 import React from 'react';
 import style from './style.module.scss';
 
 export function SuccessLeft({ nextButton }) {
-
-    const onSubmit = (cb) => {
-        cb?.();
-    };
-    return (
-        <React.Fragment>
-            <div className={style.success}>
-               <h3> You have successfully subscribed. You'll receive text to activate your license.</h3>
-               <img src="..\img\success.png"/>
-            </div>
-            <div className={style.contentArea__bottom}>
-                {nextButton({ onClick: onSubmit })}
-            </div>
-        </React.Fragment>
-    );
-};
+	const onSubmit = (cb) => {
+		cb?.();
+	};
+	return (
+		<React.Fragment>
+			<div className={style.success}>
+				<h3>
+					You have successfully subscribed. You'll receive text to
+					activate your license.
+				</h3>
+				<img src='..\img\success.png' />
+			</div>
+			<div className={style.contentArea__bottom}>
+				{nextButton({ onClick: onSubmit })}
+			</div>
+		</React.Fragment>
+	);
+}
 
 export function SuccessRight({ config: { image } }) {
-    return (
-        <div className={style.contentBlock__right}>
-            <div className={style.planArea}>
-                <div className={style.planArea__img}><img src='..\img\winner.png' /></div>
-                <ul>
-                    <li>Winner</li>
-                </ul>
-            </div>
-        </div>
-    );
+	return (
+		<div className={style.contentBlock__right}>
+			<div className={style.planArea}>
+				<div className={style.planContent}>
+					<div className={style.planContent__img}>
+						<img src='..\img\winner.png' />
+					</div>
+					<ul className={style.planContent__list}>
+						<li>Winner</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	);
 }
