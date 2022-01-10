ac44508ee5c1fe72e44631624c69bc7bc5870bb7
Basil Reji <basilreji@factweavers.com>
2022-01-10 12:00:30 +0530

    make layout reusable

--- src/Routes/Home/InfoForm.js
+++ src/Routes/Home/InfoForm.js
@@ -1,123 +1,115 @@
 /*eslint-disable*/
 import { message } from 'antd';
-import React, { useRef, useState } from 'react';
-import { submitUserPlanInfo } from '../../api';
+import React, { useState } from 'react';
 import InputField from '../../Components/InputField';
+import { RightSection } from '../../Components/Wrapper';
 import { getReqBodyFromConfig, validateInfoForm } from './helper';
 import style from './style.module.scss';
-import { SupportLink } from './SupportLink';
+import RightImage from '../../../public/img/details.png';
 
 const FormConfig = {
-	email: {
-		value: '',
-		error: '',
-	},
-	name: {
-		value: '',
-		error: '',
-	},
-	phone: {
-		value: '',
-		error: '',
-	},
+  email: {
+    value: '',
+    error: '',
+  },
+  name: {
+    value: '',
+    error: '',
+  },
+  phone: {
+    value: '',
+    error: '',
+  },
 };
 
 export function InfoFormLeft({ nextButton, submitUserInfo }) {
-	const [formConfig, setFormConfig] = useState(Object.assign({}, FormConfig));
-	const [isTouched, setTouched] = useState(false);
-	const [apiInfo, setApiInfo] = useState({ error: false, loading: false });
+  const [formConfig, setFormConfig] = useState(Object.assign({}, FormConfig));
+  const [isTouched, setTouched] = useState(false);
+  const [apiInfo, setApiInfo] = useState({ error: false, loading: false });
 
-	const { email, phone, name } = formConfig;
+  const { email, phone, name } = formConfig;
 
-	const onSubmit = async (cb) => {
-		setTouched(true);
-		const { isValid, formConfig: config } = validateInfoForm(formConfig);
-		setFormConfig({ ...config });
-		if (isValid) {
-			setApiInfo({ loading: true });
-			const { success } = await submitUserInfo(
-				getReqBodyFromConfig(formConfig)
-			);
-			setApiInfo({ error: !success });
-			if (success) cb?.();
-			else
-				message.error(
-					'We are experiencing technical difficulties, please try again later!',
-					5
-				);
-		}
-	};
+  const onSubmit = async (cb) => {
+    setTouched(true);
+    const { isValid, formConfig: config } = validateInfoForm(formConfig);
+    setFormConfig({ ...config });
+    if (isValid) {
+      setApiInfo({ loading: true });
+      const { success } = await submitUserInfo(
+        getReqBodyFromConfig(formConfig)
+      );
+      setApiInfo({ error: !success });
+      if (success) cb?.();
+      else
+        message.error(
+          'We are experiencing technical difficulties, please try again later!',
+          5
+        );
+    }
+  };
 
-	const inputChange = ({ id, target }) => {
-		let config = formConfig;
-		config[id].value = target.value;
-		if (isTouched) config = validateInfoForm(config).formConfig;
-		setFormConfig({ ...config });
-	};
+  const inputChange = ({ id, target }) => {
+    let config = formConfig;
+    config[id].value = target.value;
+    if (isTouched) config = validateInfoForm(config).formConfig;
+    setFormConfig({ ...config });
+  };
 
-	return (
-		<React.Fragment>
-			<form>
-				<ul className={style.formsBlock__list}>
-					<li>
-						<InputField
-							htmlForName='text'
-							placeholderLabel='Name'
-							type='text'
-							id='name'
-							onChange={inputChange}
-							value={name.value}
-							error={name.error}
-							required
-						/>
-					</li>
-					<li>
-						<InputField
-							htmlForName='tel'
-							placeholderLabel='Phone'
-							type='tel'
-							id='phone'
-							onChange={inputChange}
-							value={phone.value}
-							error={phone.error}
-							required
-						/>
-					</li>
-					<li>
-						<InputField
-							htmlForName='email'
-							placeholderLabel='Email'
-							type='text'
-							id='email'
-							onChange={inputChange}
-							value={email.value}
-							error={email.error}
-							required
-						/>
-					</li>
-				</ul>
-			</form>
-			<div className={style.contentArea__bottom}>
-				{nextButton({ onClick: onSubmit, loading: apiInfo.loading })}
-			</div>
-		</React.Fragment>
-	);
+  return (
+    <React.Fragment>
+      <form>
+        <ul className={style.formsBlock__list}>
+          <li>
+            <InputField
+              htmlForName='text'
+              placeholderLabel='Name'
+              type='text'
+              id='name'
+              onChange={inputChange}
+              value={name.value}
+              error={name.error}
+              required
+            />
+          </li>
+          <li>
+            <InputField
+              htmlForName='tel'
+              placeholderLabel='Phone'
+              type='tel'
+              id='phone'
+              onChange={inputChange}
+              value={phone.value}
+              error={phone.error}
+              required
+            />
+          </li>
+          <li>
+            <InputField
+              htmlForName='email'
+              placeholderLabel='Email'
+              type='text'
+              id='email'
+              onChange={inputChange}
+              value={email.value}
+              error={email.error}
+              required
+            />
+          </li>
+        </ul>
+      </form>
+      <div className={style.controlButtons}>
+        {nextButton({ onClick: onSubmit, loading: apiInfo.loading })}
+      </div>
+    </React.Fragment>
+  );
 }
 
 export function InfoFormRight({ config: { image } }) {
-	return (
-		<div className={style.contentBlock__right}>
-			<div className={style.planArea}>
-				<div className={style.planContent}>
-					<div className={style.planContent__img}>
-						<img src='..\img\details.png' />
-					</div>
-					<ul className={style.styleplanContent__list}>
-						<li>Enter your details</li>
-					</ul>
-				</div>
-				<SupportLink/>
-			</div>
-		</div>
-	);
+  return (
+    <RightSection
+      contentimage={RightImage}
+      contentText={'Enter your details'}
+      showGetStarted={false}
+    />
+  );
 }
