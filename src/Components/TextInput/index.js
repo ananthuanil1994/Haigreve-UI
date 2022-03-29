1f0c8f99d615ac4a63c6bddd9b0685578521aad9
Joel Roy <joel.roy@factweavers.com>
2022-03-29 13:43:14 +0530

    firstname, lastname and redirection

--- src/Components/TextInput/index.js
+++ src/Components/TextInput/index.js
@@ -3,49 +3,49 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Input } from 'antd';
 export const TextInput = (props) => {
-	const inputChange = (eve) => {
-		props.onChange &&
-			props.onChange({
-				...props,
-				value: eve.target.value,
-				name: eve.target.name,
-				index: props.index,
-				formObject: props.formObject,
-				...eve,
-			});
-	};
-	return (
-		<Input
-			onChange={inputChange}
-			disabled={props.disabled}
-			placeholder={props.placeholder}
-			// defaultValue={props.defaultValue}
-			value={props.value}
-			type={props.htmlType}
-			name={props.name}
-			autoFocus={props.autoFocus}
-			key={props.key}
-			onKeyDown={props.onKeyDown}
-			onPressEnter={props.onPressEnter}
-			onClick={props.onClick}
-			id={props.id}
-			max={props.max}
-			required={props.required}
-			min={props.min}
-			onBlur={props.onBlur}
-			style={props.style}
-			className={`${props.className || ''} ${!props.isValid ? ' ' : ''}`}
-		/>
-	);
+  const inputChange = (eve) => {
+    props.onChange &&
+      props.onChange({
+        ...props,
+        value: eve.target.value,
+        name: eve.target.name,
+        index: props.index,
+        formObject: props.formObject,
+        ...eve,
+      });
+  };
+  return (
+    <Input
+      onChange={inputChange}
+      disabled={props.disabled}
+      placeholder={props.placeholder}
+      // defaultValue={props.defaultValue}
+      value={props.value}
+      type={props.htmlType}
+      name={props.name}
+      autoFocus={props.autoFocus}
+      key={props.key}
+      onKeyDown={props.onKeyDown}
+      onPressEnter={props.onPressEnter}
+      onClick={props.onClick}
+      id={props.id}
+      max={props.max}
+      required={props.required}
+      min={props.min}
+      onBlur={props.onBlur}
+      style={props.style}
+      className={`${props.className || ''} ${!props.isValid ? ' ' : ''}`}
+    />
+  );
 };
 
 TextInput.propTypes = {
-	onChange: PropTypes.func.isRequired,
-	index: PropTypes.number,
-	disabled: PropTypes.bool,
-	placeHolder: PropTypes.string,
-	value: PropTypes.any,
-	htmlType: PropTypes.string.isRequired,
-	name: PropTypes.string.isRequired,
-	isValid: PropTypes.bool,
+  onChange: PropTypes.func.isRequired,
+  index: PropTypes.number,
+  disabled: PropTypes.bool,
+  placeHolder: PropTypes.string,
+  value: PropTypes.any,
+  htmlType: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  isValid: PropTypes.bool,
 };
--- src/api/index.js
+++ src/api/index.js
@@ -2,66 +2,76 @@ import axios from 'axios';
 const API_BASE_URL = 'https://bd.haigreve.com/api';
 const SUBSCRIPTION_PLAN_URL = API_BASE_URL + '/subscriptionPlans';
 const SAVE_CUSTOMER_URL = API_BASE_URL + '/saveCustomerDetails';
+const CONFIRM_SUBSCRIPTION_URL = API_BASE_URL + '/confirmSubscription'; //added confirmation url
 
 const DUMMY_DATA = [
-	{
-		planName: '1 Month License',
-		amount: '80',
-		id: 1,
-	},
-	{
-		planName: '3 Month License',
-		amount: '240',
-		id: 2,
-	},
-	{
-		planName: '6 Month License',
-		amount: '480',
-		id: 3,
-	},
-	{
-		planName: '1 Year License',
-		amount: '960',
-		id: 4,
-	},
+  {
+    planName: '1 Month License',
+    amount: '80',
+    id: 1,
+  },
+  {
+    planName: '3 Month License',
+    amount: '240',
+    id: 2,
+  },
+  {
+    planName: '6 Month License',
+    amount: '480',
+    id: 3,
+  },
+  {
+    planName: '1 Year License',
+    amount: '960',
+    id: 4,
+  },
 ];
 
 export const submitUserPlanInfo = async (data) => {
-	const payload = {
-		name: data.name,
-		email: data.email,
-		phoneNo: data.phone,
-		subscriptionPlan: data.plan,
-	};
-	try {
-		// await sleep(1000);
-		await axios.post(SAVE_CUSTOMER_URL, payload);
-
-		return { success: true };
-	} catch {
-		return { success: false };
-	}
+  const payload = {
+    firstName: data.firstName,
+    lastName: data.lastName,
+    email: data.email,
+    phoneNo: data.phone,
+    subscriptionPlan: data.plan,
+  };
+  // payload for confirmation url
+  const confirmPayload = {
+    first_name: data.firstName,
+    last_name: data.lastName,
+    email: data.email,
+    phone_number: data.phone,
+  };
+  try {
+    // await sleep(1000);
+    await axios.post(SAVE_CUSTOMER_URL, payload);
+    //confirmation url call
+    const response = await axios.post(CONFIRM_SUBSCRIPTION_URL, confirmPayload); // {"confirmation_link": ""}
+    return { success: true, confirmUrl: response.data['confirmation_link'] };
+  } catch {
+    return { success: false };
+  }
 };
 
 export const getAvailablePlans = async () => {
-	try {
-		// await sleep();
-		// const data = DUMMY_DATA;
-		const { data } = await axios.get(SUBSCRIPTION_PLAN_URL);
+  try {
+    // await sleep();
+    // const data = DUMMY_DATA;
+    const { data } = await axios.get(SUBSCRIPTION_PLAN_URL);
 
-		return {
-			success: true,
-			data,
-		};
-	} catch {
-		return { success: false };
-	}
+    return {
+      success: true,
+      data,
+    };
+  } catch {
+    return { success: false };
+  }
 };
 
 const sleep = (duration = 1000) => {
-	return new Promise((res) => {
-		return setTimeout(() => {
-			res();
-		}, duration);
-	});
+  return new Promise((res) => {
+    return setTimeout(() => {
+      res();
+    }, duration);
+  });
 };
